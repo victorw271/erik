594e600b0d052cc23f4a62b763935de81b432c55
victorw271 <73604301+victorw271@users.noreply.github.com>
2022-05-24 18:42:52 +0200

    Update analyse_datasets.py
    forgot import csv

--- mindaffectBCI/decoder/analyse_datasets.py
+++ mindaffectBCI/decoder/analyse_datasets.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with pymindaffectBCI.  If not, see <http://www.gnu.org/licenses/>
 
+import csv
 import numpy as np
 import sklearn
 from mindaffectBCI.decoder.datasets import get_dataset
